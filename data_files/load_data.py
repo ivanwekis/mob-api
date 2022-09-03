50c9c27f69d47df08febe7598e910bf552b2ead9
Ivan Moreno <ivanmoreno@gmail.com>
2022-09-03 12:45:18 +0200

    Data files and new requests

--- data_files/load_data.py
+++ data_files/load_data.py
@@ -1,4 +1,4 @@
-from multiprocessing.connection import Client
+from queue import Empty
 from pymongo import MongoClient
 import argparse
 
@@ -8,30 +8,35 @@ def read_csv(input_file):
         data = csv_file.read()
         data_list = data.split("\n")
         data = None
-        header = data_list[0].split(";")
-        data_list_dictionary = []
+        header_aux = data_list[0].split(",")
+        header=[]
+        for word in header_aux:
+            header.append(word.replace("\n", "").replace('"', ''))
 
-        for row in range(1, len(data_list) - 1):
+        data_list_dictionary = []
+        for row in range(1, len(data_list)-1):
             dictionary_aux = {}
-            row_list = data_list[row].split(";")
+            row_list = data_list[row].split(",")
             for column in range(len(header)):
-                dictionary_aux[header[column]] = row_list[column].replace("\n", "")
+                if len(header) == len(row_list):
+                    dictionary_aux[header[column]] = row_list[column].replace("\n", "").replace('"', '')
             data_list_dictionary.append(dictionary_aux)
         return data_list_dictionary
 
 
-def write_csv(data_list_dictionary):
+def write_csv(input_file, data_list_dictionary):
     MONGO_URI = "mongodb://localhost"
     client = MongoClient(MONGO_URI)
     db = client["weatherdata"]
-    collection = db[data_list_dictionary[0].get("PROVINCIA")]
+    collection = db[str(input_file).replace(".csv","")]
     for dict in data_list_dictionary:
-        collection.insert_one(dict)
+        if not dict is Empty:
+            collection.insert_one(dict)
 
 
 def main(input_file):
     data_list_dictionary = read_csv(input_file)
-    write_csv(data_list_dictionary)
+    write_csv(input_file,data_list_dictionary)
 
 
 if __name__ == "__main__":
@@ -39,7 +44,6 @@ if __name__ == "__main__":
     parser.add_argument(
         "--input_file",
         type=str,
-        default="data_files/tests.csv",
         help="Name of the input file. By default ='data_files/tests.csv'",
     )
     args = parser.parse_args()
--- src/flask_app/api_provincies/provincies.py
+++ src/flask_app/api_provincies/provincies.py
@@ -1,23 +1,51 @@
-from flask import request, jsonify, Blueprint
+from flask import request, jsonify, Blueprint, Response
 from werkzeug.security import generate_password_hash
 from classes.auth import create_key
 from pymongo import MongoClient
-
+from datetime import datetime
+from bson import json_util
 
 provincies = Blueprint('provincies',__name__)
 MONGO_URI = "mongodb://localhost"
 client = MongoClient(MONGO_URI)
 db = client["weatherdata"]
 
-@provincies.route("/test", methods=["GET"])
-def test():
-    return "mensaje recibido"
+
+@provincies.route("/api.download/<date>/<provincie>", methods=["POST"])
+def download_data_provincie(date,provincie):
+    email = request.json["email"]
+    key = request.json["key"]
+    user = db.users.find_one({"email":email,"key":key})
+    if not user is None: 
+        try:
+            datetime.strptime(date,"%Y-%m-%d")
+            collection = db.get_collection(date)
+            information = collection.find({"Provincia": provincie})
+            response  = json_util.dumps(information)
+            return Response(response, mimetype="application/json")
+        except ValueError:
+            return jsonify({"response": "Incorrect date format. example:'/2022-12-12' "}) 
+    else: 
+        return jsonify({"response": "The email or key are incorrect."})
 
 
-@provincies.route("/api.download/<email>/<key>", methods=["POST"])
-def download_data(email, key):
-
-    return "Mensaje recibido"
+@provincies.route("/api.download/<date>", methods=["POST"])
+def download_data(date):
+    email = request.json["email"]
+    key = request.json["key"]
+    user = db.users.find_one({"email":email,"key":key})
+    if not user is None: 
+        try:
+            print("User: {} is correct." .format(email))
+            datetime.strptime(date,"%Y-%m-%d")
+            collection = db.get_collection(date)
+            information = collection.find()
+            response  = json_util.dumps(information)
+            return Response(response, mimetype="application/json")
+        except ValueError:
+            return jsonify({"response": "Incorrect date format. example:'/2022-12-12' "}) 
+    else: 
+        return jsonify({"response": "The email or key are incorrect."})
 
 
 @provincies.route("/new_user", methods=["POST"])
